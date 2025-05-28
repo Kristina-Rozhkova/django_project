bf2136b10e4a8f6bbe8135728a773130fe2d33a4
Kristina-Rozhkova <rozkovakristina67@gmail.com>
2025-05-29 00:51:40 +0400

    Homework 26.1 Forms done.

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
 from django.contrib import messages
 from catalog.models import Contact, Product
+from .forms import ProductForm
 
 
 class ProductListView(ListView):
@@ -24,13 +25,13 @@ class ProductDetailView(DetailView):
 
 class ProductCreateView(CreateView):
     model = Product
-    fields = ['name', 'description', 'image', 'category', 'price']
+    form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
 
 
 class ProductUpdateView(UpdateView):
     model = Product
-    fields = ['name', 'description', 'image', 'category', 'price']
+    form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
 
     def get_success_url(self):
